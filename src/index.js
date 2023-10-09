5d26cc9467dea37ad6e6a61154ab6fab35266f83
Ash Cleave <ashley.cleave@bbc.co.uk>
2023-10-09 17:07:49 +0100

    Stop running ahead

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const getOdds = (numbers) => numbers.filter((num) => num % 2 === 1);
-const getEvens = (numbers) => numbers.filter((num) => num % 2 === 0);
 const split = (numbers) => numbers.filter((num) => num % 2 === 0);
 
 export { getOdds, getEvens, split };
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,4 @@
-import { split, getOdds } from "../src";
-
-describe("getOdds", function () {
-  it("should return odd numbers from an array of numbers", () => {
-    const numbers = [1, 2, 3, 4];
-    expect(getOdds(numbers)).toEqual([1, 3]);
-  });
-});
+import { split } from "../src";
 
 describe("splits numbers into evens and odss", function () {
   it("should return addition of two numbers", () => {
